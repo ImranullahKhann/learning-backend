85332f830bf125e0fa6050a9831c51fa8297d334
ImranullahKhann <imranullahkhann2004@gmail.com>
2025-05-27 09:27:10 +0500

    First refactor of Scrimba Node Project 1.

--- scrimba/node/1/server.js
+++ scrimba/node/1/server.js
@@ -1,5 +1,6 @@
 import http from "node:http"
 import { getDataFromDB } from './database/db.js'
+import sendResponse from "./utils/sendResponse.js"
 
 const PORT = 8000
 
@@ -7,23 +8,17 @@ const PORT = 8000
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
     if (req.url === '/api' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end(JSON.stringify(destinations))
-    } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+        sendResponse(res, 200, destinations)
+    } else if (req.url.startsWith('/api/continent/') && req.method === 'GET') {
         const cont = req.url.replace("/api/continent/", "")
         const filteredDestinations = destinations.filter((dest) => dest.continent.toLowerCase() == cont.toLowerCase())
-        res.setHeader("Content-Type", "application/json")
-        res.statusCode = 200
-        res.end(JSON.stringify(filteredDestinations))
+
+        sendResponse(res, 200, filteredDestinations)
     } else {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 404
-        res.end(JSON.stringify({
+        sendResponse(res, 404, {
                 error: "not found",
                 message: "The requested route does not exist"
-            })
-        )
+        }) 
     }
 })
 
